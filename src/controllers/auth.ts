e9674362ef7964ab0a148c2e617f984fa3e3c1d2
Veasna <Veasna3d@gmail.com>
2024-08-23 12:00:20 +0700

    User Sign-Up

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,31 @@
 import { Request, Response } from "express"
+import { prismaClient } from "..";
+import { hashSync } from 'bcrypt'
 
 export const Login = (req: Request, res: Response) => {
     res.send("Login successful")
 }
+
+export const Signup = async (req: Request, res: Response) => {
+    const { email, password, name } = req.body;
+
+    try {
+        let user = await prismaClient.user.findFirst({ where: { email } });
+
+        if (user) {
+            return res.status(400).json({ error: "User already exists" });
+        }
+
+        user = await prismaClient.user.create({
+            data: {
+                name,
+                email,
+                password: hashSync(password, 10),
+            },
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { Login } from "../controllers/auth";
+import { Login, Signup } from "../controllers/auth";
 
 const authRoutes = Router();
 
 authRoutes.get('/login', Login)
+authRoutes.post('/signup', Signup)
 
 export default authRoutes;
